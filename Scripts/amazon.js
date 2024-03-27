7fb80e78a1ca410d5b0bddccc304f3b9d1bb969a
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-03-27 16:41:38 +0530

    amazon script added

---
+++ Scripts/amazon.js
@@ -0,0 +1,32 @@
+const products = [{
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+
+    name:  'Black and Gray Athletic Cotton Socks - 6 Pairs',
+
+    rating: {
+        stars : 4.5,
+        count: 87
+    },
+    
+    priceCents : 1090
+
+}, {
+
+    image : 'images/products/intermediate-composite-basketball.jpg',
+
+    name : 'Intermediate Size Basketball',
+
+    rating: {
+        stars: 4,
+        count: 127
+    },
+    priceCents: 2095
+
+}, {
+
+
+
+
+
+}];
+
