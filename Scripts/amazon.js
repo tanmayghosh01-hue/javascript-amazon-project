75d08d19ed5a7c44216b5a684e5772a3c4dab383
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-04-16 12:53:31 +0530

    Make the cart quantity interactive

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -95,6 +95,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     //   productName: productName,
     //   quantity : 1
     // })
-    console.log(cart)
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    // console.log(cartQuantity);
+    // console.log(cart);
   });
 });
