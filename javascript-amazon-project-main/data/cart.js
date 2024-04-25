8905f75b8f244787db1af4822ceca0b1cc912db8
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-04-25 20:54:35 +0530

    Save cart in localStorage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,11 +1,25 @@
 
-export let cart = [{
-    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-}, {
-    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity : 1,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+
+if (!cart) {
+  cart = [{
+              productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+              quantity: 2,
+          }, {
+              productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+              quantity : 1,
+          }];
+}
+
+
+
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 
 export function addToCart(productId) {
@@ -33,6 +47,8 @@ export function addToCart(productId) {
           quantity : quantity
         });
       }
+
+      saveToStorage();
   }
 
 
@@ -49,5 +65,7 @@ export function addToCart(productId) {
 
 
     cart = newCart;
+
+    saveToStorage();
       
   }
