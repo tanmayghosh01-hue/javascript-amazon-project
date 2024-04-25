10691ed4c44b85ec9887d1d8a5a636ebe78dafa7
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-04-25 20:26:46 +0530

    Delete products from Html when clicking delete

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -29,7 +29,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML += `
 
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
         Delivery date: Tuesday, June 21
         </div>
@@ -125,6 +125,13 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
+
+
+            const container = document.querySelector(
+                `.js-cart-item-container-${productId}`
+            );
+
+            container.remove()
             
         });
     });
