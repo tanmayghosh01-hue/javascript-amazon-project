3d309e7b31e0439a1b451019daefba5b492b3cdb
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-05-23 01:22:55 +0530

    create oop version of cart

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -3,11 +3,13 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 
-import { cart } from "../javascript-amazon-project-main/data/cart.js";
+// import { cart } from "../javascript-amazon-project-main/data/cart.js";
+
+import "../javascript-amazon-project-main/data/cart-oop.js";
 
 
 renderOrderSummary();
 
 renderPaymentSummary();
 
-console.log(cart)
+// console.log(cart)
