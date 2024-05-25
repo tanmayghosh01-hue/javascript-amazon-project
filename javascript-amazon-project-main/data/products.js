b7271268a883d9f6174ddd440fd06e2ad48cee37
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-05-25 23:53:17 +0530

    Learn more details about classes and this

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -68,6 +68,47 @@ class Clothing extends Product {
 
 }
 
+// const date = new Date();
+// console.log(date);
+
+// console.log(date.toLocaleTimeString());
+
+/*
+console.log(this);
+
+const object2 = {
+  a : 2,
+  b : this.a
+}
+
+*/
+
+/*
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+
+object3.method();
+
+
+
+function logThis() {
+  console.log(this);
+}
+
+logThis();
+
+logThis.call('hello');
+
+*/
+
+
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
