e922fc4bae1060dca9b23dc9db3b55f8b7134050
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-05-28 18:36:25 +0530

    Add backend practice files

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -2,6 +2,8 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
+import '../javascript-amazon-project-main/data/backend-practice.js';
+
 
 // import { cart } from "../javascript-amazon-project-main/data/cart.js";
 
---
+++ javascript-amazon-project-main/data/backend-practice.js
@@ -0,0 +1,12 @@
+
+
+
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+})
+
+xhr.open('GET', 'https://supersimplebackend.dev/images/apple.jpg');
+
+xhr.send();
