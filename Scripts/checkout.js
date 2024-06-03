4cb39f51a7fe2089c88f4c705617c8ee504f708e
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-06-03 12:51:47 +0530

    Convert form promises to async and await

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -13,7 +13,23 @@ import { loadCart } from "../javascript-amazon-project-main/data/cart.js";
 
 // import "../javascript-amazon-project-main/data/cart-class.js";
 
+async function loadpage() {
+    await loadProductsFetch();
 
+
+    const value = await new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value3');
+        });
+    });
+
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+
+loadpage();
+
+/*
 Promise.all([
 
     loadProductsFetch(),
@@ -28,6 +44,8 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+*/
 
 
 /*
